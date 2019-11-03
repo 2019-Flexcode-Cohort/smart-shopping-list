177098853573f12e813cbd706ee5b06ddf3f793b
jonellelancaster <jonellelancaster@gmail.com>
2019-11-03 16:07:53 -0500

    jpaWiringTest

--- src/main/java/com/lazygrocer/smartshoppinglist/Ingredient.java
+++ src/main/java/com/lazygrocer/smartshoppinglist/Ingredient.java
@@ -1,6 +1,5 @@
 package com.lazygrocer.smartshoppinglist;
 
-import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -9,46 +8,42 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Meal {
+public class Ingredient {
 
 	private String name;
-	private int servingCount;
-	@ManyToMany
-	private List<Ingredient> ingredients;
+	private int quantity;
 	@Id
 	@GeneratedValue
-	private Long id;
+	private long id;
+	
+	@ManyToMany(mappedBy="ingredients")
+	private List<Meal> meals;
 
-	public Meal(String name, int servingCount, Ingredient... ingredients) {
+	public Ingredient(String name, int quantity) {
 		this.name = name;
-		this.servingCount = servingCount;
-		this.ingredients = Arrays.asList(ingredients);
+		this.quantity = quantity;
 
 	}
-	protected Meal() {}
+	protected Ingredient () {}
 
 	public String getName() {
 		return name;
 	}
 
-	public int getServingCount() {
-		return servingCount;
+	public int getQuantity() {
+		return quantity;
 	}
 
-	public List<Ingredient> getIngredients() {
-		return ingredients;
-	}
-
-	public Long getId() {
+	public long getId() {
 		return id;
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + servingCount;
+		result = prime * result + quantity;
 		return result;
 	}
 	@Override
@@ -59,20 +54,18 @@ public class Meal {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Meal other = (Meal) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
+		Ingredient other = (Ingredient) obj;
+		if (id != other.id)
 			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
-		if (servingCount != other.servingCount)
+		if (quantity != other.quantity)
 			return false;
 		return true;
 	}
+	
 
 }
