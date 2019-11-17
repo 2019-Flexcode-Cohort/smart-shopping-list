92839f4f0703a09ee00015e496fa7a828adaeabf
Kelley Landis <kelley.landis@outlook.com>
2019-11-17 13:41:09 -0500

    Added X

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,16 +6,22 @@ function renderIngredientInput(element) {
 }
 
 function createIngredientInput() {
-  const ingredientInput = document.createElement("section");
+  const ingredientInput = document.createElement("li");
   ingredientInput.appendChild(createNameBox());
   ingredientInput.appendChild(createNumberBox());
   const button = createAddItemButton();
+  const removeButton = createRemoveItemButton();
   ingredientInput.appendChild(button);
+  ingredientInput.appendChild(removeButton);
   button.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event.target);
-    renderIngredientInput(event.target.parentNode);
+    renderIngredientInput(event.target.parentNode.parentNode);
     event.target.remove();
+  })
+  removeButton.addEventListener('click', (event) => {
+    console.log(event.target);
+    event.target.parentNode.remove()
   })
   return ingredientInput;
 }
@@ -35,4 +41,10 @@ function createAddItemButton() {
   button.innerText = '+';
   button.classList.add('add-ingredient-button');
   return button;
+}
+function createRemoveItemButton() {
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'x';
+  removeButton.classList.add('remove-ingredient-button');
+  return removeButton;
 }
