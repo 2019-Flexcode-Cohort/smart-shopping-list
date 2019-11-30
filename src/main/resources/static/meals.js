51979ac5d53496a56d64138d0d3b6ae66a70d1df
Michael <mhathaw1@gmail.com>
2019-11-30 13:09:58 -0500

    button wiring successes

--- src/main/resources/static/meals.js
+++ src/main/resources/static/meals.js
@@ -1,13 +1,36 @@
+const increaseButton = document.querySelector(".increaseButton");
+incrementMealCount(increaseButton);
+
 function incrementMealCount() {
-    const plusButton = document.getElementsByName("plus");
-    plusButton.addEventListener("click", addOne(){
-        //Function that increases # of that meal in the MealPlan and on the home page
-    })
-    
-
+    const mealIds = document.querySelectorAll(".incrementMealId");
+    for (let i = 0; i < mealIds.length; i++){
+        mealIds[i].parentElement.addEventListener("click", () => {
+            fetch('http://localhost:8080/api/mealPlan/increaseCount/' + mealIds[i].value, {
+                method: 'GET'
+            })
+        })
+    }
 }
-
+const decreaseButton = document.querySelector(".decreaseButton");
+decrementMealCount(decreaseButton);
 
 function decrementMealCount() {
-    const minusButton = document.getElementsByName("minus");
+    const mealIds = document.querySelectorAll(".decrementMealId");
+    for (let i = 0; i < mealIds.length; i++){
+        mealIds[i].parentElement.addEventListener("click", () => {
+            fetch('http://localhost:8080/api/mealPlan/decreaseCount/' + mealIds[i].value, {
+                method: 'GET'
+            })
+        })
+    }
+}
+
+const currentMealCount = document.querySelector(".currentMealCount");
+updateCurrentMealCount(currentMealCount);
+
+async function updateCurrentMealCount() {
+    const mealCounts = document.querySelectorAll(".currentMealCount");
+    for (let i = 0; i < mealCounts.length; i++) {
+        mealCounts[i].innerText = await fetch('http://localhost:8080/api/mealPlan/mealCount/' + mealCounts[i].querySelector(".countMealId").value).body
+    }
 }
